f280271ea3a1e2d8dccd5472795ccf578cecb1ab
Changdae <einsvision@gmail.com>
2021-02-27 22:26:21 +0900

    project setting

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Twitter Clone!🚀✨🎈⭐</h1>
     </div>
   );
