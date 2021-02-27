d2ede375c76ffbb3a5a38daf35212cfb54473e1a
Changdae <einsvision@gmail.com>
2021-02-28 00:51:31 +0900

    built sidebar

--- twitter-clone/src/Sidebar.js
+++ twitter-clone/src/Sidebar.js
@@ -5,27 +5,35 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import ListAltIcon from '@material-ui/icons/ListAlt';
+// import MailOutlineIcon from '@material-ui/icons/MailOutline';
+// import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+// import ListAltIcon from '@material-ui/icons/ListAlt';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { Button } from '@material-ui/core';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <TwitterIcon />
 
-      <SidebarOption text='Home' Icon={HomeIcon}/>
+      <SidebarOption active text='Home' Icon={HomeIcon}/>
       <SidebarOption text='Expoore' Icon={SearchIcon}/>
       <SidebarOption text='Notification' Icon={NotificationsNoneIcon}/>
-      <SidebarOption text='Messages' Icon={MailOutlineIcon}/>
+      {/* <SidebarOption text='Messages' Icon={MailOutlineIcon}/>
       <SidebarOption text='Bookmarks' Icon={BookmarkBorderIcon}/>
-      <SidebarOption text='Lists' Icon={ListAltIcon}/>
+      <SidebarOption text='Lists' Icon={ListAltIcon}/> */}
       <SidebarOption text='Profile' Icon={PermIdentityIcon}/>
       <SidebarOption text='More' Icon={MoreHorizIcon}/>
-      
+
       {/* Button -> Tweet */}
+      <Button 
+        variant='outlined' 
+        className='sidebar__tweet'
+        fullWidth 
+      >
+        Tweet
+      </Button>
     </div>
   )
 }
