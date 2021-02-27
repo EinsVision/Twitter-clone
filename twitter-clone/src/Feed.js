2dbfd970be3073eb1f860f7ff5de63f10895fb74
Changdae <einsvision@gmail.com>
2021-02-28 01:32:36 +0900

    scrollbar display none

--- twitter-clone/src/Feed.js
+++ twitter-clone/src/Feed.js
@@ -5,6 +5,36 @@ function Feed() {
   return (
     <div className='feed'>
       <h2 className='feed__header'>Home</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
+      <h2>Test</h2>
       
       {/* Header */}
       {/* TweetBox */}
