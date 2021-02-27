28a354562d34606a31beaa80acb4f21f0623dae2
Changdae <einsvision@gmail.com>
2021-02-28 01:09:38 +0900

    logo stying and made structure

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import Feed from './Feed';
 import Sidebar from './Sidebar';
 
 function App() {
@@ -6,7 +8,7 @@ function App() {
     <div className="app">
       <Sidebar />
       
-      {/* Feed */}
+      <Feed />
       
       {/* Widgets */}
     </div>
---
+++ twitter-clone/src/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Feed.css'
+
+function Feed() {
+  return (
+    <div className='feed'>
+      <h2>Feed </h2>
+      {/* Header */}
+      {/* TweetBox */}
+
+      {/* Post */}
+      {/* Post */}
+    </div>
+  )
+}
+
+export default Feed
