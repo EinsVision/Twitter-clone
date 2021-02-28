176a1e73c46d6ee6eab151d14621d3dd39493623
Changdae <einsvision@gmail.com>
2021-02-28 21:36:52 +0900

    installed npm i react-twitter-embed

--- twitter-clone/src/Widgets.js
+++ twitter-clone/src/Widgets.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Widgets.css';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
 
 function Widgets() {
   return (
