726decfce3cd3c8535e7bc9445faa0221ce3413b
Changdae <einsvision@gmail.com>
2021-02-28 22:14:17 +0900

    building widgets using npm twitter embed

--- twitter-clone/src/Widgets.js
+++ twitter-clone/src/Widgets.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Widgets.css';
 import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Widgets() {
   return (
     <div className='widgets'>
-      <h2>Hello widgets</h2>
+      <div className='widgets__input'>
+        <SearchIcon className='widgets__searchIcon' />
+        <input placeholder='Search Twitter' type='text'/>
+      </div>
+
+      <div className='widgets__widgetContainer'>
+        <h2>What's happening?</h2>
+        <TwitterTweetEmbed tweetId={"1366007374499090433"} />
+
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='YhUx7CpfW7fPaNT'
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={'www.einsvision.tk'}
+          option={{text: '#react is awesome', via: 'einsvision'}} 
+        />
+      </div>
     </div>
   )
 }
