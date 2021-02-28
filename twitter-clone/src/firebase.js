b84cf20e1257d6cb7788a83c287e353cf06ace52
Changdae <einsvision@gmail.com>
2021-02-28 22:34:08 +0900

    setup db of firebase

--- twitter-clone/src/firebase.js
+++ twitter-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAQJELTEnT19sk9VxkKxE27bmwgEIpzWjY",
   authDomain: "twitter-clone-5a659.firebaseapp.com",
@@ -6,3 +8,9 @@ const firebaseConfig = {
   messagingSenderId: "314558956977",
   appId: "1:314558956977:web:2466e425d20e267feea990"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
