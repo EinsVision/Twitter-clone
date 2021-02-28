a5d26c585f89d7cad8a4c61c25170a885001c2ff
Changdae <einsvision@gmail.com>
2021-02-28 22:31:12 +0900

    installed firebase

---
+++ twitter-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQJELTEnT19sk9VxkKxE27bmwgEIpzWjY",
+  authDomain: "twitter-clone-5a659.firebaseapp.com",
+  projectId: "twitter-clone-5a659",
+  storageBucket: "twitter-clone-5a659.appspot.com",
+  messagingSenderId: "314558956977",
+  appId: "1:314558956977:web:2466e425d20e267feea990"
+};
